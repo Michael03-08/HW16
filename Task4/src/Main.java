239decdbd6fc694a212c22c5cca93740e695606d
Flex <cma491628@gmail.com>
2023-12-23 15:06:54 +0200

    регулярные выражения

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static private final String VALID_LINE = "\\+\\d{11}";
+    public static final String NUM_TEST = "(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
     public static void main(String[] args) {
         //todo Numbers
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите номер телефона: ");
         String number = scanner.nextLine().trim();
+        number = number.replaceAll("\\D+", " ");
 
-       if(number.matches(VALID_LINE)){
+       if(number.length() == 11){
+           number =number.replaceAll("(\\d)" + NUM_TEST, "+7 ($2) $3 $4-S5");
            System.out.println(number);
-        }else {
+        } else if (number.length() == 10) {
+           number = number.replaceAll(NUM_TEST, "+7 ($1) $2 $3-$4");
+           System.out.println(number);
+       } else {
            System.out.println("Не верный номер");
        }
     }
